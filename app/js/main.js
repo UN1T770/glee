8c1e05f888d3769020784d2e32c4312b3e767ba8
UN1T770 <il1ya.volchenko@yandex.ru>
2021-04-17 15:31:44 +0800

    added product1

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,28 @@
-$(function(){
+$(function () {
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  })
 
   $('.top-slider__inner').slick({
-  
     dots: true,
     arrows: false
   });
+
+  $(".start").rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true
+  });
+
 });
